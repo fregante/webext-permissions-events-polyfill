7babfe6dbce5e8b0a755bdf4bf4ea2b82c87cf6b
Federico Brigante <github@bfred.it>
2019-07-04 14:52:26 +0800

    Add permission removal

--- tests/background.js
+++ tests/background.js
@@ -40,7 +40,7 @@ test('onAdded: false', async t => {
 
 test('onAdded: true', async t => {
 	const listener = new Promise(resolve => {
-		chrome.permissions.onAdded.addListener(resolve);
+		browser.permissions.onAdded.addListener(resolve);
 	});
 	const user = userCall('ALLOW the permission request', () =>
 		browser.permissions.request({
@@ -50,4 +50,34 @@ test('onAdded: true', async t => {
 	t.true(await user);
 	t.deepEqual(await listener, onlyYouTubePermission);
 	t.deepEqual(await browser.permissions.getAll(), jointPermissions);
+});
+
+test('onAdded: true (already there)', async t => {
+	browser.permissions.onAdded.addListener(t.fail);
+	const user = browser.permissions.request({
+		origins: ['https://youtube.com/*']
+	});
+	t.true(await user);
+	t.deepEqual(await browser.permissions.getAll(), jointPermissions);
+});
+
+test('onRemoved: true', async t => {
+	const listener = new Promise(resolve => {
+		browser.permissions.onRemoved.addListener(resolve);
+	});
+	const request = browser.permissions.remove({
+		origins: ['https://youtube.com/*']
+	});
+	t.true(await request);
+	t.deepEqual(await listener, onlyYouTubePermission);
+	t.deepEqual(await browser.permissions.getAll(), onlyGooglePermission);
+});
+
+test('onRemoved: true (already not there)', async t => {
+	browser.permissions.onAdded.addListener(t.fail);
+	const request = browser.permissions.remove({
+		origins: ['https://youtube.com/*']
+	});
+	t.true(await request);
+	t.deepEqual(await browser.permissions.getAll(), onlyGooglePermission);
 });
